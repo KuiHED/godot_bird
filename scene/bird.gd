99ba6952a3b13bac691e458944c952f7c87c250a
KuiHED <wkui666@gmai.com>
2022-12-31 15:28:41 +0800

    鸟和测试场景做完

--- scene/bird.gd
+++ scene/bird.gd
@@ -21,30 +21,33 @@ func _ready():
 	set_max_contacts_reported(1)
 	connect("body_entered",Callable(self,"on_body_entered_event"))
 	pass
-
-func _process(delta):
+func _physics_process(delta):
+#func _process(delta):
 	if rotation < deg_to_rad(-30):
 		rotation = deg_to_rad(-30)
 		set_angular_velocity(0)
 	if get_linear_velocity().y > 0:
 		set_angular_velocity(3)
-	
+	if rotation > deg_to_rad(80):
+		rotation = deg_to_rad(80)
 	if Input.is_action_just_pressed("fly_button"):
 		set_linear_velocity(Vector2(get_linear_velocity().x, -200))
-		set_angular_velocity(-3)
+		set_angular_velocity(-5)
 		set_gravity_scale(0)
 		animated.play()
 		$wing.play()
-		
-#	if(animated.frame == 2):
-#		animated.stop()
-#		animated.frame = 0
-#		set_gravity_scale(gravityScale)
+	
+	if(animated.frame == 2):
+		animated.stop()
+		animated.frame = 0
+		set_gravity_scale(gravityScale)
+		set_linear_velocity(Vector2(speed, get_linear_velocity().y))
 		
 	position.y = clamp(position.y, 0, screen_size.y)
 	pass
 	
-func on_body_entered_event():
+func on_body_entered_event(other_body):
+	print("撞击")
 	$hit.play()
 	hp -= 1
 	pass
