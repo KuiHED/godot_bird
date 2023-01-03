384c606d47ae1be3bae589cfc4faedf48fac4fc8
KuiHED <wkui666@gmai.com>
2023-01-03 22:23:18 +0800

    完整循环做完。从开始到结束计分再到开始。

---
+++ scene/game.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+@onready var camera2d: Camera2D = $Camera2D
+@onready var bird: RigidBody2D = $Bird
+
+@onready var background: Node2D = $Background
+@onready var pipes: Node2D = $Pipes
+
+signal end_game(point: int)
+
+var pipeInterval: int = 150
+var pipeCount: int = 3
+
+func _ready():
+	#以当前小鸟的位置，每隔pipeInterval间距生成水管
+	for i in range(20):
+		print(i)
+		createPipe()
+	pass
+	
+func _process(delta):
+	#移动相机
+	camera2d.position.x = bird.position.x
+	#移动多少个屏幕背景
+	var count = int(bird.position.x) / 1152
+	background.position.x = count * 1152
+	#血量小于0则结束游戏
+	if (bird.hp <= 0):
+		emit_signal("end_game", bird.point)
+	pass
+
+func createPipe():
+	var createPositionX = pipeCount * pipeInterval
+	var createPositionY = randf_range(150, 400)
+	
+	var pipeResource = preload("res://scene/pipe.tscn")
+	var newPipe = pipeResource.instantiate()
+	newPipe.position.x = createPositionX
+	newPipe.position.y = createPositionY
+	newPipe.name = String.num_int64(pipeCount)
+	pipes.add_child(newPipe)
+	
+	newPipe.get_node("VisibleOnScreenNotifier2D").screen_exited.connect(onPipeScreenExited.bind(newPipe))
+#	newPipe.get_node("Coin").connect("body_entered", Callable(self, "onBirdEntered"))
+	newPipe.get_node("Coin").body_entered.connect(onBirdEntered)
+	pipeCount += 1
+	pass
+
+func onPipeScreenExited(exitedPipe: Node2D):
+#	var time = get_tree().create_timer(2)
+#	await time.timeout
+	exitedPipe.queue_free()
+	createPipe()
+	pass
+
+func onBirdEntered(other_body):
+	if (other_body == bird):
+		$Bird/point.play()
+		bird.point += 1
+	pass
---
+++ scene/main.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+func _ready():
+	home()
+	pass
+
+func home():
+	var homeResource = preload("res://scene/home.tscn")
+	var home = homeResource.instantiate()
+	add_child(home)
+	$Home/Control/Start.connect("pressed",Callable(self,"startGame"))
+	$swoosh.play()
+	if (get_node_or_null("Over") != null):
+		$Over.queue_free()
+	pass
+	
+
+func startGame():
+	var gameResoure = preload("res://scene/game.tscn")
+	var game = gameResoure.instantiate()
+	add_child(game)
+	
+	game.end_game.connect(endGame)
+	
+	$Home.queue_free()
+	
+	$swoosh.play()
+	pass
+
+func endGame(point: int):
+	var overResource = preload("res://scene/over.tscn")
+	var over = overResource.instantiate()
+	add_child(over)
+	$Over/Control/Score.text = String.num_int64(point)
+	$Over/Control/Menu.connect("pressed", Callable(self, "home"))
+	$Game.queue_free()
+	
+	$die.play()
+	pass
